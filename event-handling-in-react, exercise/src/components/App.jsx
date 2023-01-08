704fc7ae3b9695fec5fcf59d69127ac23b9f49a8
Dominik Maciazek <114284333+DominikMaciazek@users.noreply.github.com>
2023-01-08 14:18:06 +0100

    Update App.jsx

--- event-handling-in-react, exercise/src/components/App.jsx
+++ event-handling-in-react, exercise/src/components/App.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 
 function App() {
-  const [headingText, setHeadingText] = useState("Hello");
+  const [headingText, setHeadingText] = useState("");
   const [isMouseOver, setMouseOver] = useState(false);
+  const [name, setName] = useState("");
 
-  function handleClick() {
-    setHeadingText("Submitted");
+  function handleClick(event) {
+    setHeadingText(name);
   }
 
   function handleMouseOver() {
@@ -16,10 +17,18 @@ function App() {
     setMouseOver(false);
   }
 
+  function handleChange(event) {
+    setName(event.target.value);
+  }
+
   return (
     <div className="container">
-      <h1>{headingText}</h1>
-      <input type="text" placeholder="What's your name?" />
+      <h1>Hello {headingText}</h1>
+      <input
+        onChange={handleChange}
+        type="text"
+        placeholder="What's your name?"
+      />
       <button
         onClick={handleClick}
         style={{ backgroundColor: isMouseOver ? "black" : "white" }}
@@ -33,3 +42,4 @@ function App() {
 }
 
 export default App;
+
